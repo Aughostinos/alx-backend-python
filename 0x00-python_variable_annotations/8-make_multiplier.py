5e8472fe12feaa7adbeed50cb3c953e93938b903
Augustinos <augustinos.nabil@gmail.com>
2024-08-15 22:55:48 +0000

    task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -7,7 +7,7 @@ def make_multiplier(multiplier: float) -> callable[[float], float]:
     """ a type-annotated function make_multiplier that takes a float
     multiplier as argument and returns a function that multiplies a
     float by multiplier"""
-    
+
     def multiplication(val: float) -> float:
         return val * multiplier
     return multiplication
