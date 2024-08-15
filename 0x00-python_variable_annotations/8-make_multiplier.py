71c7abe1b83e0fefb187b59803f6d269e4cca138
Aughostinos <augustinos.nabil@gmail.com>
2024-08-16 01:55:27 +0300

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Task 8. Complex types - functions"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> callable[[float], float]:
+    """ a type-annotated function make_multiplier that takes a float
+    multiplier as argument and returns a function that multiplies a
+    float by multiplier"""
+    
+    def multiplication(val: float) -> float:
+        return val * multiplier
+    return multiplication
