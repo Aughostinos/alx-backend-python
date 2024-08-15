27a3e8346e49ec2639a28ef3caa5f47827933289
Aughostinos <augustinos.nabil@gmail.com>
2024-08-16 01:09:12 +0300

    task6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Task 6. Complex types - mixed list"""
+from typing import List
+
+
+def sum_mixed_list (mxd_lst: List[float, int]) -> float:
+    """ a type-annotated function sum_mixed_list which takes a list
+    mxd_lst of integers and floats and returns their sum as a float."""
+    sum: float = 0
+    for element in mxd_lst :
+        sum += element
+    return sum
