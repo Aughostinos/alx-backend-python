fb2836963ee72fa8c2e40297d3438444a7e92de8
Augustinos <augustinos.nabil@gmail.com>
2024-08-15 19:38:42 +0000

    fix

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,4 +5,4 @@
 def concat(str1: str, str2: str) -> str:
     """ a type-annotated function concat that takes a string str1 and a
     string str2 as arguments and returns a concatenated string"""
-    return "{} {}".format(str1, str2)
+    return str1 + str2 
