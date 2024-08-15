d91d43236b40d50215393ff423e6465fe4254ec6
Augustinos <augustinos.nabil@gmail.com>
2024-08-15 23:46:39 +0000

    task9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,6 +2,8 @@
 """Task 9. Let's duck type an iterable object"""
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """Annotating function’s parameters and return values with the appropriate types"""
+    """Annotating function’s parameters and return values with the
+    appropriate types"""
     return [(i, len(i)) for i in lst]
