dc9b507925d4d13074aa8ad431d74ec9a53b20d9
Augustinos <augustinos.nabil@gmail.com>
2024-08-16 01:30:41 +0000

    task10

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,6 +2,7 @@
 """Task 10. Duck typing - first element of a sequence"""
 from typing import Sequence, Any, Union
 
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """The types of the elements of the input are not know"""
     if lst:
