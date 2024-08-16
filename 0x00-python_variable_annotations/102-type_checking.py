b2df95f2f06d2571b0289d3057b34ad21252b529
Aughostinos <augustinos.nabil@gmail.com>
2024-08-16 05:03:33 +0300

    fix

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,10 +3,10 @@
 from typing import Tuple, List
 
  
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
+def zoom_array(lst: Tuple[int], factor: int = 2) -> List[int]:
     """fix error: Incompatible types in assignment
     (expression has type "List[Any]", variable has type "Tuple[Any, ...]")"""
-    zoomed_in: List = [
+    zoomed_in: List[int] = [
         item for item in lst
         for i in range(factor)
     ]
