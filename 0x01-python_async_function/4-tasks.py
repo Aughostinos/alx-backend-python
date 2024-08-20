62031daa4c398a14eb982ac5a288791a902f2c68
Aughostinos <augustinos.nabil@gmail.com>
2024-08-20 05:41:42 +0300

    task4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Task 4: Altering wait_n into task_wait_n"""
+
+import asyncio
+import heapq
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    The code is nearly identical to wait_n except task_wait_random
+    is being called.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    heapq.heapify(delays)
+    return [heapq.heappop(delays) for _ in range(len(delays))]
