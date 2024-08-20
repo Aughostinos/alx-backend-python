b2b913f05e2d6ce5b87d4031509c9e2cce3764e9
Augustinos <augustinos.nabil@gmail.com>
2024-08-20 02:34:53 +0000

    fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,13 +7,14 @@ import heapq
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """an async routine called wait_n that takes in 2 int arguments
     (in this order): n and max_delay. You will spawn wait_random n
     times with the specified max_delay. wait_n should return the list
     of all the delays (float values). The list of the delays should be
     in ascending order without using sort() because of concurrency."""
-    
+
     delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
     heapq.heapify(delays)
     return [heapq.heappop(delays) for _ in range(len(delays))]
