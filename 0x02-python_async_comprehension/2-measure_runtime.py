abc3b59c92c96190e00b74588164cb391036a19d
Aughostinos <augustinos.nabil@gmail.com>
2024-08-21 04:48:10 +0300

    fix

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,7 +6,7 @@ from typing import List
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> List[float]:
+async def measure_runtime() -> float:
     """coroutine that will execute async_comprehension four times
     in parallel using asyncio.gather. measure_runtime should measure
     the total runtime and return it."""
