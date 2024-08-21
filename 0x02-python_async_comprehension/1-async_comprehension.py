e78e1790b03f32641156b826dfa1dc3d5e46ce98
Aughostinos <augustinos.nabil@gmail.com>
2024-08-21 04:21:11 +0300

    task1 fix

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,5 +5,7 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    result = [value async for value in async_generator()]
-    print(result)
+    """coroutine will collect 10 random numbers using an async
+    comprehensing over async_generator,
+    then return the 10 random numbers."""
+    return [value async for value in async_generator()]
