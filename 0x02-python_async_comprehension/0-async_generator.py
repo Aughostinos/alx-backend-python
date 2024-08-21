501018813eac22fcf41e447922df953311210de8
Augustinos <augustinos.nabil@gmail.com>
2024-08-21 00:50:54 +0000

    make the file executable

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,5 +6,9 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float]:
+    """coroutine called async_generator that takes no arguments.
+    The coroutine will loop 10 times, each time asynchronously wait
+    1 second, then yield a random number between 0 and 10."""
+
     asyncio.sleep(1)
     random.uniform(1, 10)
