455fcb9bc621863799d6ffbf31123687673a3c52
Aughostinos <augustinos.nabil@gmail.com>
2024-09-10 01:42:21 +0300

    fix code style

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -26,11 +26,11 @@ class TestGithubOrgClient(unittest.TestCase):
             f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, {"key": "value"})
 
-    
     @patch("client.GithubOrgClient.org", new_callable=property)
     def test_public_repos_url(self, mock_org):
         """Test that _public_repos_url returns the expected repos URL."""
-        mock_org.return_value = {"repos_url": "https://api.github.com/orgs/test-org/repos"}
+        mock_org.return_value = (
+            {"repos_url": "https://api.github.com/orgs/test-org/repos"})
 
         # Create an instance of the GithubOrgClient
         client = GithubOrgClient("test-org")
@@ -38,13 +38,14 @@ class TestGithubOrgClient(unittest.TestCase):
         # Check if the _public_repos_url returns the correct URL
         result = client._public_repos_url
         self.assertEqual(result, "https://api.github.com/orgs/test-org/repos")
-        
+
     @patch("client.get_json")
     @patch("client.GithubOrgClient._public_repos_url", new_callable=property)
     def test_public_repos(self, mock_public_repos_url, mock_get_json):
         """Test public_repos method."""
         # Define the mocked return values
-        mock_public_repos_url.return_value = "https://api.github.com/orgs/test-org/repos"
+        mock_public_repos_url.return_value = (
+            "https://api.github.com/orgs/test-org/repos")
         mock_get_json.return_value = [
             {"name": "repo1"},
             {"name": "repo2"},
@@ -62,8 +63,9 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(repos, expected_repos)
 
         mock_public_repos_url.assert_called_once()
-        mock_get_json.assert_called_once_with("https://api.github.com/orgs/test-org/repos")
-        
+        mock_get_json.assert_called_once_with(
+            "https://api.github.com/orgs/test-org/repos")
+
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False),
