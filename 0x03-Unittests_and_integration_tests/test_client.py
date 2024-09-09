16e38e905d43d0f0cae9a64034f58455fad901e5
Aughostinos <augustinos.nabil@gmail.com>
2024-09-10 01:27:46 +0300

    task 4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+4. Parameterize and patch as decorators
+"""
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test cases for GithubOrgClient"""
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch("client.get_json")
+    def test_org(self, org_name, mock_get_json):
+        """Test GithubOrgClient.org"""
+        mock_get_json.return_value = {"key": "value"}
+        client = GithubOrgClient(org_name)
+
+        result = client.org
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")
+        self.assertEqual(result, {"key": "value"})
+
+
+if __name__ == '__main__':
+    unittest.main()
